0798e5f726db58e51d51336ea7bb7de20306bad2
MiroHack0410 <emirbaxin@gmail.com>
2025-12-06 09:11:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const lanchaRoutes = require('./routes/lanchaRoutes');
 const cors = require('cors');
+const lanchaRoutes = require('./routes/lanchaRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -9,12 +8,17 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Rutas
 app.use('/api', lanchaRoutes);
 
-// Servidor
+// Mensaje raíz
+app.get("/", (req, res) => {
+    res.send("Backend funcionando correctamente 🚤");
+});
+
+// Inicializar servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
