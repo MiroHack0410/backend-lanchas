b42da95c85644b7a514e4b5a58c67724d80fdaf7
MiroHack0410 <emirbaxin@gmail.com>
2025-12-07 17:48:53 -0600

    Add usuarioRoutes to server configuration

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const lanchaRoutes = require('./routes/lanchaRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Inicializar BD
 require('./models/initDB');
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rutas API
 app.use('/api', lanchaRoutes);
+app.use('/api', usuarioRoutes);
 
-// Ruta principal
 app.get("/", (req, res) => {
     res.send("Backend funcionando correctamente 🚤");
 });
 
-// Servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
