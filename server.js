571993720e17ef6d19f01d935636e50dd986fc22
MiroHack0410 <emirbaxin@gmail.com>
2025-12-07 19:02:05 -0600

    Initialize database connection and update comments
    Added database connection initialization and improved comments.

--- server.js
+++ server.js
@@ -7,18 +7,22 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Inicializar conexión a BD
 require('./models/initDB');
 
 app.use(cors());
 app.use(express.json());
 
+// Rutas
 app.use('/api', lanchaRoutes);
 app.use('/api', usuarioRoutes);
 
+// Ruta principal
 app.get("/", (req, res) => {
     res.send("Backend funcionando correctamente 🚤");
 });
 
+// Iniciar servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
